a0c41e778292c7ce2b26c181ca00ffddd8eee4c5
Ashwin Ponnur <127082257+ashwin-p@users.noreply.github.com>
2025-02-09 17:38:04 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -25,3 +25,20 @@ def load_checkpoint(model, optimizer, filename, device='cuda'):
 def save_generated_images(images, epoch, output_dir):
     grid = torchvision.utils.make_grid(images)
     torchvision.utils.save_image(grid, os.path.join(output_dir, f'epoch_{epoch}.png'))
+
+def print_model_parameters(model):
+    print(f"{'Layer':<30}{'Shape':<30}{'Parameters':<15}")
+    print("=" * 75)
+    
+    train_params = 0
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            num_params = param.numel()
+            train_params += num_params
+            print(f"{name:<30}{str(list(param.shape)):<30}{num_params:<15}")
+
+    print("=" * 75)
+    print(f"Total Trainable Parameters: {train_params:,}")
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"Total Parameters: {total_params:,}")
+    
